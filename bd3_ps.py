338a183fd07c61ff7fe52906ab93d58f0a2db033
David Frye <DFrye333@gmail.com>
2012-12-11 23:33:45 -0800

    Initial commit.

---
+++ bd3_ps.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import socket
+
+open_ports = []
+min_port = 20
+max_port = 1025
+
+def print_header():
+    print("================ BD3PS ================")
+
+def print_footer():
+    print("=======================================")
+
+def print_open():
+    print("Open Ports:")
+    for x in open_ports:
+        print("\t *", x)
+
+def main():
+    os.system("cls")
+    print_header()
+    if 1 < len(sys.argv):
+        targetHost = sys.argv[1]
+    else:
+        targetHost = input("Enter host to scan:")
+    targetIP = socket.gethostbyname(targetHost)
+    #scan reserved ports
+    for i in range(min_port, max_port):
+        os.system("cls")
+        print_header()
+        print("Begin scan on host:", targetIP)
+        print("Scanning port:", i, "...")
+        print_open()
+        print_footer()
+#        s = socket(AF_INET, SOCK_STREAM)
+        s = socket.socket()
+        result = s.connect_ex((targetIP, i))
+        if 0 == result:
+            open_ports.append(i)
+        s.close()
+    print_header()
+    print("End scan on host:", targetIP)
+    print_open()
+
+main()
