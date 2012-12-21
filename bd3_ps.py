533afec2b7a44455cfd52332a160ede719651d53
David <david@david-laptop.(none)>
2012-12-20 19:53:23 -0800

    Added clear_screen() for portability.

--- bd3_ps.py
+++ bd3_ps.py
@@ -7,48 +7,54 @@ import socket
 open_ports = []
 
 def print_header():
-    print("==================== BD3PS ====================")
+	print("==================== BD3PS ====================")
 
 def print_footer():
-    print("===============================================")
+	print("===============================================")
 
 def print_open():
-    print("Open Ports:")
-    for x in open_ports:
-        print("\t *", x)
+	print("Open Ports:")
+	for x in open_ports:
+		print("\t *", x)
+
+def clear_screen():
+	if("posix" == os.name):
+		os.system("clear");
+	elif("nt" == os.name):
+		os.system("cls");
 
 def main():
-    min_port = 0
-    max_port = 1023
+	min_port = 0
+	max_port = 1023
 
-    os.system("cls")
-    print_header()
-    if 3 < len(sys.argv):
-        min_port = int(sys.argv[2])
-        max_port = int(sys.argv[3])
-    elif 2 < len(sys.argv):
-        max_port = int(sys.argv[2])
-    if 1 < len(sys.argv):
-        targetHost = sys.argv[1]
-    else:
-        targetHost = input("Enter host to scan:")
-    targetIP = socket.gethostbyname(targetHost)
-    for i in range(min_port, max_port):
-        os.system("cls")
-        print_header()
-        print("Begin scan on host:", targetIP, "from", min_port, "to", max_port)
-        print("Scanning port: ", i, "...", sep = "")
-        print_open()
-        print_footer()
-        s = socket.socket()
-        result = s.connect_ex((targetIP, i))
-        if 0 == result:
-            open_ports.append(i)
-        s.close()
-    os.system("cls")
-    print_header()
-    print("End scan on host:", targetIP, "from", min_port, "to", max_port)
-    print_open()
-    print_footer()
+	clear_screen();
+	print_header()
+	if 3 < len(sys.argv):
+		min_port = int(sys.argv[2])
+		max_port = int(sys.argv[3])
+	elif 2 < len(sys.argv):
+		max_port = int(sys.argv[2])
+	if 1 < len(sys.argv):
+		targetHost = sys.argv[1]
+	else:
+		targetHost = input("Enter host to scan:")
+	targetIP = socket.gethostbyname(targetHost)
+	for i in range(min_port, max_port):
+		clear_screen();
+		print_header()
+		print("Begin scan on host:", targetIP, "from", min_port, "to", max_port)
+		print("Scanning port: ", i, "...", sep = "")
+		print_open()
+		print_footer()
+		s = socket.socket()
+		result = s.connect_ex((targetIP, i))
+		if 0 == result:
+			open_ports.append(i)
+		s.close()
+	clear_screen();
+	print_header()
+	print("End scan on host:", targetIP, "from", min_port, "to", max_port)
+	print_open()
+	print_footer()
 
 main()
